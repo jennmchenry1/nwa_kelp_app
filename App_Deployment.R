641cdca903fc9f1e50528bf8b71388c2e9246c4f
unknown <jennmchenry1@gmail.com>
2026-06-23 09:39:54 -0700

    fixing directory length

--- App_Deployment.R
+++ App_Deployment.R
@@ -2,16 +2,16 @@ library(rsconnect)
 
 
 
-setwd("C:/Users/Jenn/Dropbox/GITHUB/RShiny_Repos/nwa_kelp_app/")
+setwd("C:/Users/Jenn/Dropbox/GITHUB/RShiny_Repos/")
 
 # https://www.shinyapps.io/admin/#/dashboard
 rsconnect::setAccountInfo(name='jennifermchenry',
                           token='C58D5A16C20AC8045B89CB73F1483BFE',
                           secret='l54+Yu5uGU1qY/viBa3oUI2iIbKPogBQ+E55kiUJ')
 
-# rsconnect::deployApp(appName = "nwa_kelp_app",account = "jennifermchenry",appDir = "nwa_kelp_app")  
+rsconnect::deployApp(appName = "nwa_kelp_app",account = "jennifermchenry",appDir = "nwa_kelp_app")
 
 
-rsconnect::configureApp(appName = "nwa_kelp_app", size="large",account = "jennifermchenry")
+rsconnect::configureApp(appName = "nwa_kelp_app", size="large",account = "jennifermchenry",appDir = "nwa_kelp_app")
 
 
